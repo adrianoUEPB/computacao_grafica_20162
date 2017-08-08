e3795fb2576f4d5696b2db43cb89d506b6915a9f
adria <adria@DESKTOP-LQV4FKU>
2017-08-08 02:32:15 -0300

    Adicionado processamento de imagens

--- src/view/Main.java
+++ src/view/Main.java
@@ -2,7 +2,8 @@ package view;
 
 public class Main {
 	public static void main(String[] args) {
-		new MenuDeOp();		
+		new MenuDeOp();	
+//		new ProcessamentoDeImagens();
 //		janela2D.comboBox.setModel(new DefaultComboBoxModel(new String[] {"PIXEL", "RETA", "CIRCUNFERENCIA", "ELIPSE"}));
 	}
 }
